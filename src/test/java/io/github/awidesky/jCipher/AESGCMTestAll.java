ea39e7ed8e6cbe5fd32f75c23ec98ac719705553
Eugene Hong <awidesky@naver.com>
2023-05-28 23:56:23 +0900

    fix test

--- src/test/java/io/github/awidesky/jCipher/AESGCMTestAll.java
+++ src/test/java/io/github/awidesky/jCipher/AESGCMTestAll.java
@@ -45,27 +45,30 @@ import io.github.awidesky.jCipher.messageInterface.MessageProvider;
  * Please refer to LICENSE
  * */
 
-@DisplayName("AES GCM ALL")
+@DisplayName("AES GCM")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AESGCMTestAll {
 
 	public static final Charset TESTCHARSET = Charset.forName("UTF-16"); // TODO : param test? 
 	static SecureRandom ran = new SecureRandom();
 	static AESGCMCipher c = new AESGCMCipher(8 * 1024);
+	
+	static byte[] src;
+	static String randomStr;
+	
+	@BeforeAll
+	static void setupData() {
+		src = new byte[64 * 1024];
+		randomStr = "random String 1234!@#$";
+		ran.nextBytes(src);
+	}
+	
 
 	@Nested
-	@DisplayName("Decryption")
+	@Order(1)
+	@DisplayName("Encryption")
 	@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 	class Encryption {
-		
-		static byte[] src;
-		static String randomStr;
-		
-		@BeforeAll
-		static void setupData() {
-			src = new byte[64 * 1024];
-			randomStr = "random String 1234!@#$";
-			ran.nextBytes(src);
-		}
 		
 		@BeforeEach
 		void beforeEach() {
@@ -116,7 +119,7 @@ class AESGCMTestAll {
 		void string() throws IllegalBlockSizeException, BadPaddingException, FileNotFoundException, IOException,
 				DigestException, NoSuchAlgorithmException {
 			assertEquals(TestUtil.hashPlain(randomStr.getBytes(TESTCHARSET)), TestUtil.testDecrypt(
-					new ByteArrayInputStream(c.encryptFromString(MessageProvider.from(randomStr, TESTCHARSET)))));
+					new ByteArrayInputStream(c.encryptToSingleBuffer(MessageProvider.from(randomStr, TESTCHARSET)))));
 		}
 
 		@DisplayName("Inputstream -> Outputstream")
@@ -172,6 +175,7 @@ class AESGCMTestAll {
 	}
 
 	@Nested
+	@Order(2)
 	@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 	@DisplayName("Decryption")
 	class Decryption {
@@ -181,9 +185,11 @@ class AESGCMTestAll {
 		
 		@BeforeAll
 		static void setup() throws NoSuchAlgorithmException, DigestException, IOException, IllegalBlockSizeException, BadPaddingException {
-			srcHash = TestUtil.hashPlain(encrypted);
-			encrypted = c.encryptToSingleBuffer(MessageProvider.from(encrypted));
+			c.init(TestUtil.password);
+			encrypted = c.encryptToSingleBuffer(MessageProvider.from(src));
+			srcHash = TestUtil.hashPlain(src);
 		}
+		
 		@BeforeEach
 		void beforeEach() {
 			c.init(TestUtil.password);
@@ -194,8 +200,8 @@ class AESGCMTestAll {
 		@Test
 		void byteArr() throws IllegalBlockSizeException, BadPaddingException, FileNotFoundException, IOException,
 				DigestException, NoSuchAlgorithmException {
-			assertEquals(TestUtil.hashPlain(encrypted),
-					TestUtil.testDecrypt(new ByteArrayInputStream(c.encryptToSingleBuffer(MessageProvider.from(encrypted)))));
+			assertEquals(srcHash,
+					TestUtil.hashPlain(c.decryptToSingleBuffer(MessageProvider.from(encrypted))));
 		}
 
 		@DisplayName("File -> File")
@@ -206,7 +212,7 @@ class AESGCMTestAll {
 			File fsrc = mkTempEncryptedFile();
 			File fdest = mkEmptyTempFile();
 			c.encrypt(MessageProvider.from(fsrc), MessageConsumer.to(fdest));
-			assertEquals(TestUtil.hashPlain(new FileInputStream(fsrc)), TestUtil.testDecrypt(new FileInputStream(fdest)));
+			assertEquals(srcHash, TestUtil.hashPlain(new FileInputStream(fdest)));
 		}
 
 		@DisplayName("Base64 encoded String -> Base64 encoded String ")
@@ -214,8 +220,8 @@ class AESGCMTestAll {
 		@Test
 		void base64() throws IllegalBlockSizeException, BadPaddingException, FileNotFoundException, IOException,
 				DigestException, NoSuchAlgorithmException {
-			String result = c.encryptToBase64(MessageProvider.fromBase64(Base64.getEncoder().encodeToString(encrypted)));
-			assertEquals(TestUtil.hashPlain(encrypted), TestUtil.testDecrypt(new ByteArrayInputStream(Base64.getDecoder().decode(result))));
+			String result = c.decryptToBase64(MessageProvider.fromBase64(Base64.getEncoder().encodeToString(encrypted)));
+			assertEquals(srcHash, TestUtil.hashPlain(Base64.getDecoder().decode(result)));
 		}
 
 		@DisplayName("Hex encoded String -> Hex encoded String")
@@ -223,65 +229,32 @@ class AESGCMTestAll {
 		@Test
 		void hexStr() throws IllegalBlockSizeException, BadPaddingException, FileNotFoundException, IOException,
 				DigestException, NoSuchAlgorithmException {
-			String result = c.encryptToHexString(MessageProvider.fromHexString(HexFormat.of().formatHex(encrypted)));
-			assertEquals(TestUtil.hashPlain(encrypted), TestUtil.testDecrypt(new ByteArrayInputStream(HexFormat.of().parseHex(result))));
-		}
-
-		@DisplayName("String -> byte[]")
-		@Order(5)
-		@Test
-		void string() throws IllegalBlockSizeException, BadPaddingException, FileNotFoundException, IOException,
-				DigestException, NoSuchAlgorithmException {
-			String randomStr = "";
-			assertEquals(TestUtil.hashPlain(randomStr.getBytes(TESTCHARSET)), TestUtil.testDecrypt(
-					new ByteArrayInputStream(c.encryptToSingleBuffer(MessageProvider.from(randomStr, TESTCHARSET)))));
+			String result = c.decryptToHexString(MessageProvider.fromHexString(HexFormat.of().formatHex(encrypted)));
+			assertEquals(srcHash, TestUtil.hashPlain(HexFormat.of().parseHex(result)));
 		}
 
 		@DisplayName("Inputstream -> Outputstream")
-		@Order(6)
+		@Order(5)
 		@Test
 		void inputstream() throws IllegalBlockSizeException, BadPaddingException, FileNotFoundException, IOException,
 				DigestException, NoSuchAlgorithmException {
 			File fsrc = mkTempEncryptedFile();
 			File fdest = mkEmptyTempFile();
-			c.encrypt(MessageProvider.from(new BufferedInputStream(new FileInputStream(fsrc))),
+			c.decrypt(MessageProvider.from(new BufferedInputStream(new FileInputStream(fsrc))),
 					MessageConsumer.to(new BufferedOutputStream(new FileOutputStream(fdest))));
-			assertEquals(TestUtil.hashPlain(new FileInputStream(fsrc)), TestUtil.testDecrypt(new FileInputStream(fdest)));
-		}
-
-		@DisplayName("part of InputStream -> byte[]")
-		@Order(7)
-		@Test
-		void inputstream_partial() throws IllegalBlockSizeException, BadPaddingException, FileNotFoundException,
-				IOException, DigestException, NoSuchAlgorithmException {
-			File fsrc = mkTempEncryptedFile();
-			assertEquals(TestUtil.hashPlain(new FileInputStream(fsrc), 16 * 1024),
-					TestUtil.testDecrypt(new ByteArrayInputStream(c.encryptToSingleBuffer(
-							MessageProvider.from(new BufferedInputStream(new FileInputStream(fsrc)), 16 * 1024)))));
+			assertEquals(srcHash, TestUtil.hashPlain(new FileInputStream(fdest)));
 		}
 
 		@DisplayName("ReadableByteChannel -> WritableByteChannel")
-		@Order(8)
+		@Order(6)
 		@Test
 		void channel() throws IllegalBlockSizeException, BadPaddingException, FileNotFoundException, IOException,
 				DigestException, NoSuchAlgorithmException {
 			File fsrc = mkTempEncryptedFile();
 			File fdest = mkEmptyTempFile();
-			c.encrypt(MessageProvider.from(FileChannel.open(fsrc.toPath(), StandardOpenOption.READ)),
+			c.decrypt(MessageProvider.from(FileChannel.open(fsrc.toPath(), StandardOpenOption.READ)),
 					MessageConsumer.to(FileChannel.open(fdest.toPath(), StandardOpenOption.WRITE)));
-			assertEquals(TestUtil.hashPlain(new FileInputStream(fsrc)),
-					TestUtil.testDecrypt(new FileInputStream(fdest)));
-		}
-
-		@DisplayName("From part of ReadableByteChannel")
-		@Order(9)
-		@Test
-		void channel_partial() throws IllegalBlockSizeException, BadPaddingException, FileNotFoundException,
-				IOException, DigestException, NoSuchAlgorithmException {
-			File fsrc = mkTempEncryptedFile();
-			assertEquals(TestUtil.hashPlain(new FileInputStream(fsrc), 16 * 1024),
-					TestUtil.testDecrypt(new ByteArrayInputStream(
-							c.encryptToSingleBuffer(MessageProvider.from(FileChannel.open(fsrc.toPath(), StandardOpenOption.READ), 16 * 1024)))));
+			assertEquals(srcHash, TestUtil.hashPlain(new FileInputStream(fdest)));
 		}
 
 		static File mkTempEncryptedFile() throws IOException {
