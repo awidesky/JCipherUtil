371e0fe1b10a1f6aec2ff7d47d62378bee237ba7
Eugene Hong <awidesky@naver.com>
2023-06-25 17:34:38 +0900

    add package-info

---
+++ src/main/java/io/github/awidesky/jCipher/cipher/symmetric/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package provide actual concrete {@link io.github.awidesky.jCipher.CipherUtil} interface's subclasses that uses symmetric key. 
+ * 
+ * @author Eugene Hong
+ * 
+ */
+package io.github.awidesky.jCipher.cipher.symmetric;
