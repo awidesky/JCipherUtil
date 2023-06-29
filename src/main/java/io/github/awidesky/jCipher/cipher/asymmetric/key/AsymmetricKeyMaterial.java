e9deae867a5b6e4b57f143945e14dd8da3ba13eb
Eugene Hong <awidesky@naver.com>
2023-06-29 21:31:29 +0900

    change versio number to SNAPSHOT & fix comment

--- src/main/java/io/github/awidesky/jCipher/cipher/asymmetric/key/AsymmetricKeyMaterial.java
+++ src/main/java/io/github/awidesky/jCipher/cipher/asymmetric/key/AsymmetricKeyMaterial.java
@@ -52,7 +52,7 @@ public class AsymmetricKeyMaterial {
 	}
 
 	/**
-	 * Generate {@link javax.crypto.KeyPair}.
+	 * Generate {@link KeyPair}.
 	 * If there's already a {@code KeyPair} previously generated, or this {@code AsymmetricKeyMaterial} is constructed with a {@code PublicKey},
 	 * {@code PrivateKey} or {@code KeyPair}, return it.
 	 * Else, new {@code KeyPair} will be generated.
