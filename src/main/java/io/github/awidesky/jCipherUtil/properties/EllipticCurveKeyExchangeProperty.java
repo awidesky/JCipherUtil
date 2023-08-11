f98deb8ffe4543ef7d71a8b73cbd1488d3c23015
Eugene Hong <awidesky@naver.com>
2023-08-11 16:30:28 +0900

    add comment

--- src/main/java/io/github/awidesky/jCipherUtil/properties/EllipticCurveKeyExchangeProperty.java
+++ src/main/java/io/github/awidesky/jCipherUtil/properties/EllipticCurveKeyExchangeProperty.java
@@ -1,20 +1,29 @@
 package io.github.awidesky.jCipherUtil.properties;
 
+import java.security.KeyPairGenerator;
+
+import javax.crypto.KeyAgreement;
+
 public class EllipticCurveKeyExchangeProperty {
 
-	/** TODO : comment stub
-	 * Name of the cipher algorithm, like <code>AES</code> or <code>ChaCha20-Poly1305</code>
+	/**
+	 * Name of the Elliptic-Curve keypair generation algorithm, like <code>EC</code> or <code>XDH</code>
 	 * 
-	 * @see <a href="https://docs.oracle.com/en/java/javase/17/docs/specs/security/standard-names.html#cipher-algorithm-names">https://docs.oracle.com/en/java/javase/17/docs/specs/security/standard-names.html#cipher-algorithm-names</a>
+	 * @see KeyPairGenerator#getInstance(String)
+	 * @see <a href="https://docs.oracle.com/en/java/javase/17/docs/specs/security/standard-names.html#keypairgenerator-algorithms">https://docs.oracle.com/en/java/javase/17/docs/specs/security/standard-names.html#keypairgenerator-algorithmss</a>
 	 * */
 	public final String KEYPAIRALGORITHM;
 	/**
-	 * Name of the cipher algorithm, like <code>AES</code> or <code>ChaCha20-Poly1305</code>
+	 * Name of the Elliptic-Curve key agreement algorithm, like <code>ECDH</code> or <code>XDH</code>
 	 * 
-	 * @see <a href="https://docs.oracle.com/en/java/javase/17/docs/specs/security/standard-names.html#cipher-algorithm-names">https://docs.oracle.com/en/java/javase/17/docs/specs/security/standard-names.html#cipher-algorithm-names</a>
+	 * @see KeyAgreement#getInstance(String)
+	 * @see <a href="https://docs.oracle.com/en/java/javase/17/docs/specs/security/standard-names.html#keyagreement-algorithms">https://docs.oracle.com/en/java/javase/17/docs/specs/security/standard-names.html#keyagreement-algorithms</a>
 	 * */
 	public final String KEYAGREEMENTALGORITHM;
 	
+	/**
+	 * Construct with given keypair generation algorithm and key agreement algorithm.
+	 * */
 	public EllipticCurveKeyExchangeProperty(String keyPairAlgorithm, String keyAgreementAlgorithm) {
 		KEYPAIRALGORITHM = keyPairAlgorithm;
 		KEYAGREEMENTALGORITHM = keyAgreementAlgorithm;
